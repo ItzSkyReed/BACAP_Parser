280ae631bf0a68ca01df3b3b22945a9fbaad3546
SkyReed <itzskyreed@gmail.com>
2025-01-07 02:55:21 +0500

    Add detailed docstrings to methods in PackMCMeta class for better documentation and clarity

--- src/BACAP_Parser/PackMCMeta.py
+++ src/BACAP_Parser/PackMCMeta.py
@@ -4,7 +4,17 @@ from .utils import safe_load_json_file
 
 
 class PackMCMeta:
+    """
+    Object for managing and validating a Minecraft datapack's metadata.
+    """
     def __init__(self, datapack_path: Path):
+        """
+        :param datapack_path: A `Path` object representing the root directory of the Minecraft datapack.
+                              This directory must contain a valid `pack.mcmeta` file.
+        :raises FileNotFoundError: If `pack.mcmeta` is not found in the provided datapack directory,
+        or if the `data` directory (or an overlay directory, if specified) is missing or not a directory.
+        :raises ValueError: If `pack.mcmeta` is not a valid JSON file or is empty.
+        """
         if not (datapack_path / "pack.mcmeta").exists():
             raise FileNotFoundError("pack.mcmeta not found in the datapack root, may be this is a wrong path")
 
@@ -26,6 +36,12 @@ class PackMCMeta:
         self._description = self._parse_description()
 
     def _parse_description(self) -> str:
+        """
+        Parses and extracts the description from the `pack.mcmeta` file.
+
+        :return: A string containing the description of the datapack.
+        :raises KeyError: If the `pack.mcmeta` file does not contain a `pack` or `description` key.
+        """
         raw_description = self._json["pack"]["description"]
         if type(raw_description) == str:
             return raw_description
@@ -36,6 +52,12 @@ class PackMCMeta:
             return text
 
     def _validate_pack_format(self) -> int:
+        """
+        Validates the `pack_format` value from the `pack.mcmeta` file.
+
+        :return: An integer representing the validated `pack_format`.
+        :raises ValueError: If the `pack_format` key is missing or if the format is unsupported.
+        """
         pack_format = self._json.get("pack", {}).get("pack_format", -1)
 
         if pack_format == -1:
@@ -48,15 +70,31 @@ class PackMCMeta:
 
     @staticmethod
     def __get_min_inclusive(format_field: [int, list[int, int], dict[str, int]]) -> int | None:
+        """
+        Extracts the minimum inclusive `pack_format` from a given format field.
+
+        :param format_field: The format field.
+        :return: An integer representing the minimum inclusive `pack_format`, or `None` if the format is invalid.
+        """
         if isinstance(format_field, int):
             return format_field  # If it's just an int, return it
+
         elif isinstance(format_field, list):
             return min(format_field)  # If it's a list, take the minimum value
+
         elif isinstance(format_field, dict):
             return format_field.get("min_inclusive", None)  # If it's a dict, return min_inclusive
+
         return None  # If the format is invalid, return None
 
     def _get_max_min_overlay_directory(self) -> Path:
+        """
+        Determines the overlay directory with the highest minimum `pack_format` value.
+
+        :return: A `Path` object representing the directory of the overlay with the highest minimum inclusive format.
+        :raises ValueError: If the `overlays` dictionary does not contain `entries`, or if any entry has invalid formats.
+        :raises KeyError: If an entry is missing the `formats` dictionary or `directory` field.
+        """
         overlays = self._json.get("overlays", {})
         entries = overlays.get("entries", [])
 
@@ -83,25 +121,37 @@ class PackMCMeta:
 
         return max_min_inclusive_directory
 
-
-
-
     @property
     def path(self) -> Path:
+        """
+        :return: The path to the `pack.mcmeta` file.
+        """
         return self._path
 
     @property
     def json(self) -> dict:
+        """
+        :return: The parsed JSON content of the `pack.mcmeta` file.
+        """
         return self._json
 
     @property
     def data_path(self) -> Path:
+        """
+        :return: The path to the `data` directory or the data directory in the latest overlay in the datapack.
+        """
         return self._data_path
 
     @property
     def description(self) -> str | None:
+        """
+        :return: The description of the datapack, or ``None`` if datapack does not contain it.
+        """
         return self._description
 
     @property
     def pack_format(self) -> int:
+        """
+        :return: Pack format of the datapack.
+        """
         return self._pack_format
